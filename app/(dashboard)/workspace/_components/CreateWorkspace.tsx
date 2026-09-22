3af47f1898fb939e43b5be7218c8a5b8d7b6eac8
jabir-everest <jabir.mohammed@everest.engineering>
2026-09-22 17:43:18 +0530

    Implement authentication functionality and finalise related features

---
+++ app/(dashboard)/workspace/_components/CreateWorkspace.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Plus } from "lucide-react";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { workspaceSchema } from "@/app/schemas/workspace";
+
+
+export function CreateWorkspace() {
+  const [open, setOpen] = useState(false);
+
+  const form = useForm<z.infer<typeof workspaceSchema>>({
+    resolver: zodResolver(workspaceSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  function onSubmit() {
+    console.log("data");
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <Tooltip>
+        <TooltipTrigger
+          render={
+            <DialogTrigger
+              render={
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="size-12 border-2 border-dashed border-muted-foreground/50 
+                  text-muted-foreground hover:border-muted-foreground hover:text-foreground hover:rounded-2xl
+                  transition-all duration-200"
+                />
+              }
+            />
+          }
+        >
+          <Plus className="size-5" />
+        </TooltipTrigger>
+        <TooltipContent side="right">
+          <p>Create workspace</p>
+        </TooltipContent>
+        <DialogContent className="sm:max-w-106.25">
+          <DialogHeader>
+            <DialogTitle>Create Workspace</DialogTitle>
+            <DialogDescription>
+              Create a new workspace to get started
+            </DialogDescription>
+          </DialogHeader>
+          <form
+            id="form-create-workspace"
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="py-2"
+          >
+            <FieldGroup>
+              <Controller
+                name="name"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor="form-rhf-demo-title">Name</FieldLabel>
+                    <Input
+                      {...field}
+                      id="form-create-workspace-name"
+                      aria-invalid={fieldState.invalid}
+                      placeholder="My workspace"
+                      autoComplete="off"
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+            </FieldGroup>
+          </form>
+          <DialogFooter>
+            <Button type="submit" form="form-create-workspace">
+              Create workspace
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Tooltip>
+    </Dialog>
+  );
+}
